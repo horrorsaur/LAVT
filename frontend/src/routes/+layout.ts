f6f3b9277da94f04277efa25e2745c0b77e295f7
horrorsaur <horrorsaur@gmail.com>
2023-11-19 16:51:30 -0500

    Update Vite config and package.json

---
+++ frontend/src/routes/+layout.ts
@@ -0,0 +1,6 @@
+// Exclude routes from manifests used for dynamic SSR
+export const prerender = true
+
+// Disable server-side rendering to turn this into SPA
+// Fallback is defined in svelte.config.js
+export const ssr = false
